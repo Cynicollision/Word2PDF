b5848e8035f95590930534b1b9dbd0b886c5b05a
Sean Normoyle <stn825@gmail.com>
2014-05-08 17:33:29 -0400

    first commit
    adding files to GitHub!

---
+++ Word2PDF/Word2PDFConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Office.Interop.Word;
+using System.IO;
+using System;
+
+namespace Word2PDF
+{
+    public class Word2PDFConverter
+    {
+        private static Application wordApp = new Application();
+        private static Document doc;
+
+        private static const string ErrorMsgFileDoesNotExist = @"Selected file does not exist!";
+
+        public static void ConvertToPDF(string sourceFile)
+        {
+            if (!File.Exists(sourceFile))
+            {
+                throw new Exception(ErrorMsgFileDoesNotExist));
+            }
+        }
+    }
+}
